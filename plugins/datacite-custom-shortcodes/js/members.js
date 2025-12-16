2292c5d47f9e20fc74d8883ce5b6a46953e6697e
Kelly Stathis <kelly.a.stathis@gmail.com>
2025-12-16 14:48:24 -0800

    exclude inactive members from query to fix counts

--- plugins/datacite-custom-shortcodes/js/members.js
+++ plugins/datacite-custom-shortcodes/js/members.js
@@ -260,7 +260,7 @@ function getCountryName(countryCode) {
 function getMembers(member_type) {
     var xmlhttp = new XMLHttpRequest();
     var url =
-      "https://api.datacite.org/providers?query=-id:txvt&member-type={{member_type}}&exclude-registration-agencies=true&page[size]=400".replace("{{member_type}}", member_type);
+      "https://api.datacite.org/providers?query=-id:txvt%20AND%20is_active:%5Cu0001&member-type={{member_type}}&exclude-registration-agencies=true&page[size]=400".replace("{{member_type}}", member_type);
 
     xmlhttp.onreadystatechange = function () {
       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
